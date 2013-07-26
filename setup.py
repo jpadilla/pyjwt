2b5dd54dabe599676f3fa5b4f4c90b59856e908f
Boris Feld <lothiraldan@gmail.com>
2013-07-26 15:32:51 +0200

    Bump version
    Bump version as pypi version is already the 0.1.5, it helps when using master version with setuptools.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "PyJWT",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "Jeff Lindsay",
     author_email = "jeff.lindsay@twilio.com",
     description = ("JSON Web Token implementation in Python"),
