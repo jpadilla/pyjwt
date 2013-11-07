3bb76c110d4b43f713a5d4a63933b88ff67ac04d
Patryk Zawadzki <patrys@room-303.com>
2013-11-07 11:57:21 +0100

    Announce Python 3 support
    Also let `./setup.py test` work in Python 2

--- setup.py
+++ setup.py
@@ -2,25 +2,30 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "PyJWT",
-    version = "0.1.6",
-    author = "Jeff Lindsay",
-    author_email = "jeff.lindsay@twilio.com",
-    description = ("JSON Web Token implementation in Python"),
-    license = "MIT",
-    keywords = "jwt json web token security signing",
-    url = "http://github.com/progrium/pyjwt",
+    name="PyJWT",
+    version="0.1.6",
+    author="Jeff Lindsay",
+    author_email="jeff.lindsay@twilio.com",
+    description="JSON Web Token implementation in Python",
+    license="MIT",
+    keywords="jwt json web token security signing",
+    url="http://github.com/progrium/pyjwt",
     packages=['jwt'],
     scripts=['bin/jwt'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Utilities",
     ],
-    test_suite='tests.test_jwt'
-)
+    test_suite='tests.test_jwt')
