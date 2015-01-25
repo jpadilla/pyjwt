fca6d7e3e282a8a198dd9f3ea4390b63b7286c8f
Mark Adams <mark@markadams.me>
2015-01-25 07:14:44 -0600

    Fixed constant time compare tests so that they always use bytes as input.

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,6 +1,7 @@
 from jwt.compat import constant_time_compare
 
 from .compat import unittest
+from .utils import ensure_bytes
 
 
 class TestCompat(unittest.TestCase):
@@ -8,10 +9,16 @@ class TestCompat(unittest.TestCase):
         pass
 
     def test_constant_time_compare_returns_true_if_same(self):
-        self.assertTrue(constant_time_compare('abc', 'abc'))
+        self.assertTrue(constant_time_compare(
+            ensure_bytes('abc'), ensure_bytes('abc')
+        ))
 
     def test_constant_time_compare_returns_false_if_diff_lengths(self):
-        self.assertFalse(constant_time_compare('abc', 'abcd'))
+        self.assertFalse(constant_time_compare(
+            ensure_bytes('abc'), ensure_bytes('abcd')
+        ))
 
     def test_constant_time_compare_returns_false_if_totally_different(self):
-        self.assertFalse(constant_time_compare('abcd', 'efgh'))
+        self.assertFalse(constant_time_compare(
+            ensure_bytes('abcd'), ensure_bytes('efgh')
+        ))
