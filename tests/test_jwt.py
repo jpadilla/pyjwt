1edd4aa3578564db852cf6ccf634f40f63b4fc11
Mark Adams <mark@markadams.me>
2015-03-15 18:21:45 -0500

    Added a better explanation for TestJWT and why it exists.

--- tests/test_jwt.py
+++ tests/test_jwt.py
@@ -11,7 +11,12 @@ def utc_timestamp():
 
 
 class TestJWT(unittest.TestCase):
-
+    """
+    These tests exist primarily to ensure that calls to jwt.encode and
+    jwt.decode don't explode. Most functionality is tested by the PyJWT class
+    tests. This is primarily a sanity check to make sure we don't break the
+    public global functions.
+    """
     def setUp(self):  # noqa
         self.payload = {'iss': 'jeff', 'exp': utc_timestamp() + 15,
                         'claim': 'insanity'}
