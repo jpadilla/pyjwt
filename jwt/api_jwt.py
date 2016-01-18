0ca33af3391b9b2445df7ded3f141cac2f6e261b
Jesse Panganiban <me@jpanganiban.com>
2016-01-18 21:27:00 +0800

    `get_unverified_headers` compat with python-jose

--- jwt/api_jwt.py
+++ jwt/api_jwt.py
@@ -185,3 +185,4 @@ decode = _jwt_global_obj.decode
 register_algorithm = _jwt_global_obj.register_algorithm
 unregister_algorithm = _jwt_global_obj.unregister_algorithm
 get_unverified_header = _jwt_global_obj.get_unverified_header
+get_unverified_headers = _jwt_global_obj.get_unverified_header
