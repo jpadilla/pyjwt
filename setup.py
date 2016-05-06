d02eaad527a2b00d184eebb2d720447fdbdbb033
Mark Adams <mark@markadams.me>
2016-05-06 00:05:31 -0500

    Add Python 3.5 to CI builds

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities',
     ],
     test_suite='tests',
