c5ea68827f5dc5564bde6c087c938189cbbf1f8a
Adam Chainz <adam@adamj.eu>
2016-09-29 15:30:25 +0100

    Convert README from Markdown to reStructuredText
    PyPI only supports reStructuredText, so this won't look broken there now.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_version(package):
 
 version = get_version('jwt')
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     long_description = readme.read()
 
 if sys.argv[-1] == 'publish':
