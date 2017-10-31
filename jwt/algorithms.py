d81dd020916aeaa092d26b4d70f63f75d352a5c6
Evert Lammerts <evert@lucipher.net>
2017-10-31 22:00:27 +0100

    implementation of from_jwk for ecdsa keys

--- jwt/algorithms.py
+++ jwt/algorithms.py
@@ -1,3 +1,4 @@
+import base64
 import hashlib
 import hmac
 import json
@@ -14,7 +15,8 @@ from .utils import (
 try:
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.serialization import (
-        load_pem_private_key, load_pem_public_key, load_ssh_public_key
+        load_pem_private_key, load_pem_public_key, load_ssh_public_key,
+        load_der_public_key, load_der_private_key
     )
     from cryptography.hazmat.primitives.asymmetric.rsa import (
         RSAPrivateKey, RSAPublicKey, RSAPrivateNumbers, RSAPublicNumbers,
@@ -371,6 +373,183 @@ if has_crypto:
                 return True
             except InvalidSignature:
                 return False
+
+        @staticmethod
+        def from_jwk(jwk):
+
+            ASN1_TAG_SEQUENCE = 0b10000
+            ASN1_TAG_OBJECT_IDENTIFIER = 0b110
+            ASN1_TAG_INTEGER = 0b10
+            ASN1_TAG_BITSTRING = 0b11
+            ASN1_TAG_OCTETSTRING = 0b100
+            ASN1_CONTEXT_SPECIFIC = 0b10000000
+            ASN1_CONSTRUCTED = 0b100000
+
+            # ASN.1 Object Identifiers for EC public key and curves
+            ASN1_OID_PUBKEY = b'\x2a\x86\x48\xce\x3d\x02\x01'
+            # 1.2.840.10045.2.1
+            ASN1_OID_CURVE_P256 = b'\x2a\x86\x48\xce\x3d\x03\x01\x07'
+            # 1.2.840.10045.3.1.7
+            ASN1_OID_CURVE_P384 = b'\x2b\x81\x04\x00\x22'
+            # 1.3.132.0.34
+            ASN1_OID_CURVE_P521 = b'\x2b\x81\x04\x00\x23'
+            # 1.3.132.0.35
+
+            def encode_length(length):
+                """Object length is encoded as a single octet for lengths <=
+                127 octets and in max 1 + 127 octets for everything > 127
+                octets."""
+                res = bytearray()
+                if length <= 127:
+                    res.append(length)
+                else:
+                    lengthbytes = bytearray()
+                    while length > 0:
+                        lengthbytes.append(length & 0xFF)
+                        length = length >> 8
+                    if len(lengthbytes) > 127:
+                        raise Exception("Cannot encode objects this long in ASN.1")
+                        # Not sure what to raise here
+                    res.append(0b10000000 | len(lengthbytes))
+                    res.extend(lengthbytes)
+                return bytes(res)
+
+            try:
+                obj = json.loads(jwk)
+            except ValueError:
+                raise InvalidKeyError('Key is not valid JSON')
+
+            if obj.get('kty') != 'EC':
+                raise InvalidKeyError('Not an Elliptic curve key')
+
+            if 'x' not in obj or 'y' not in obj:
+                raise InvalidKeyError('Not an Elliptic curve key')
+
+            x = base64.urlsafe_b64decode(obj.get('x'))
+            y = base64.urlsafe_b64decode(obj.get('y'))
+
+            curve = obj.get('crv')
+            if curve == 'P-256':
+                if len(x) == len(y) == 32:
+                    curve_oid = ASN1_OID_CURVE_P256
+                else:
+                    raise InvalidKeyError("X should be 32 bytes for curve P-256")
+            elif curve == 'P-384':
+                if len(x) == len(y) == 48:
+                    curve_oid = ASN1_OID_CURVE_P384
+                else:
+                    raise InvalidKeyError("X should be 48 bytes for curve P-384")
+            elif curve == 'P-521':
+                if len(x) == len(y) == 66:
+                    curve_oid = ASN1_OID_CURVE_P521
+                else:
+                    raise InvalidKeyError("X should be 66 bytes for curve P-521")
+            else:
+                raise InvalidKeyError("Invalid curve: {}".format(curve))
+
+            ec_point = bytearray()
+            ec_point.append(ASN1_TAG_BITSTRING)
+            ec_point.extend(encode_length(2 + len(x)*2))
+            ec_point.append(0)  # no unused bits
+            ec_point.append(4)  # no compression
+            ec_point.extend(x)
+            ec_point.extend(y)
+
+            if 'd' in obj:
+                # Encode as private key.
+                #
+                # Format (https://tools.ietf.org/html/rfc5915#section-3):
+                #
+                #   ECPrivateKey ::= SEQUENCE {
+                #     version        INTEGER { ecPrivkeyVer1(1) } (ecPrivkeyVer1),
+                #     privateKey     OCTET STRING,
+                #     parameters [0] ECParameters {{ NamedCurve }} OPTIONAL,
+                #     publicKey  [1] BIT STRING OPTIONAL
+                #   }
+                d = base64.urlsafe_b64decode(obj.get('d'))
+                if len(d) != len(x):
+                    raise InvalidKeyError(
+                        "D should be {} bytes for curve {}", len(x), curve
+                    )
+
+                version = bytearray()
+                version.append(ASN1_TAG_INTEGER)
+                version.extend(encode_length(0x01))
+                version.append(1)
+
+                privatekey = bytearray()
+                privatekey.append(ASN1_TAG_OCTETSTRING)
+                privatekey.extend(encode_length(len(d)))
+                privatekey.extend(d)
+
+                parameters = bytearray()
+                parameters.append(ASN1_CONTEXT_SPECIFIC | ASN1_CONSTRUCTED)
+                # context specific, constructed because OPTIONAL [0]
+                parameters.extend(encode_length(len(curve_oid) + 2))
+                parameters.append(ASN1_TAG_OBJECT_IDENTIFIER)
+                parameters.extend(encode_length(len(curve_oid)))
+                parameters.extend(curve_oid)
+
+                publickey = bytearray()
+                publickey.append(ASN1_CONTEXT_SPECIFIC | ASN1_CONSTRUCTED | 1)
+                # context specific, constructed because OPTIONAL [1]
+                publickey.extend(encode_length(len(ec_point)))
+                publickey.extend(ec_point)
+
+                sequence_length = len(version) + len(privatekey) + \
+                                  len(parameters) + len(publickey)
+
+                ec_privatekey = bytearray()
+                ec_privatekey.append(ASN1_CONSTRUCTED | ASN1_TAG_SEQUENCE)
+                ec_privatekey.extend(encode_length(sequence_length))
+                ec_privatekey.extend(version)
+                ec_privatekey.extend(privatekey)
+                ec_privatekey.extend(parameters)
+                ec_privatekey.extend(publickey)
+
+                return load_der_private_key(
+                    bytes(ec_privatekey), None, default_backend()
+                )
+
+            # Encode as public key.
+            #
+            # Format (https://tools.ietf.org/html/rfc5480#section-2):
+            #
+            #   PublicKeyInfo ::= SEQUENCE {
+            #     algorithm       AlgorithmIdentifier,
+            #     PublicKey       BIT STRING
+            #   }
+            #
+            #   AlgorithmIdentifier ::= SEQUENCE {
+            #     algorithm       OBJECT IDENTIFIER,
+            #     parameters      ANY DEFINED BY algorithm OPTIONAL
+            #   }
+            algorithm_identifier = bytearray()
+            algorithm_identifier.append(ASN1_TAG_OBJECT_IDENTIFIER)
+            algorithm_identifier.extend(encode_length(len(ASN1_OID_PUBKEY)))
+            algorithm_identifier.extend(ASN1_OID_PUBKEY)
+            algorithm_identifier.append(ASN1_TAG_OBJECT_IDENTIFIER)
+            algorithm_identifier.extend(encode_length(len(curve_oid)))
+            algorithm_identifier.extend(curve_oid)
+
+            algorithm = bytearray()
+            algorithm.append(ASN1_CONSTRUCTED | ASN1_TAG_SEQUENCE)
+            algorithm.extend(encode_length(len(algorithm_identifier)))
+
+            sequence_length = len(algorithm_identifier) + \
+                              len(algorithm) + len(ec_point)
+
+            publickey_info = bytearray()
+            publickey_info.append(ASN1_CONSTRUCTED | ASN1_TAG_SEQUENCE)
+            publickey_info.extend(encode_length(sequence_length))
+            publickey_info.extend(algorithm)
+            publickey_info.extend(algorithm_identifier)
+            publickey_info.extend(ec_point)
+
+            return load_der_public_key(
+                bytes(publickey_info), default_backend()
+            )
+
 
     class RSAPSSAlgorithm(RSAAlgorithm):
         """
