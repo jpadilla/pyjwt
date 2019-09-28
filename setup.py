12eaf0057e431e77ddba34715d138986e7039030
Jon Dufresne <jon.dufresne@gmail.com>
2019-09-28 13:32:04 -0700

    Pass python_requires argument to setuptools (#440)
    Helps pip decide what version of the library to install. This is
    especially useful when removing support for EOL Python versions.
    
    https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
    
    > If your project only runs on certain Python versions, setting the
    > python_requires argument to the appropriate PEP 440 version specifier
    > string will prevent pip from installing the project on other Python
    > versions.
    
    https://setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
    
    > python_requires
    >
    > A string corresponding to a version specifier (as defined in PEP 440)
    > for the Python version, used to specify the Requires-Python defined in
    > PEP 345.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     test_suite='tests',
     setup_requires=pytest_runner,
     tests_require=tests_require,
