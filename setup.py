3b2d930a6acc48847b9895267f564b843243aa9d
Djailla <bastien.vallet@gmail.com>
2019-10-23 00:57:22 +0200

    Add support of python 3.8

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Utilities",
     ],
     python_requires=">=3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
