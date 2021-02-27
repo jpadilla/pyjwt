18d65916dcd7b25fe6d10c02501ee2d7b93d1870
Aaron Campbell <aaron@monkey.org>
2021-02-27 10:49:32 -0500

    Add missing exceptions.InvalidKeyError to jwt module __init__ imports.

--- jwt/__init__.py
+++ jwt/__init__.py
@@ -13,6 +13,7 @@ from .exceptions import (
     InvalidAudienceError,
     InvalidIssuedAtError,
     InvalidIssuerError,
+    InvalidKeyError,
     InvalidSignatureError,
     InvalidTokenError,
     MissingRequiredClaimError,
