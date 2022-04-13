c5dab28cefd1d2af5662aaa3cd033765984a4d91
EduardoSantos2 <86204182+EduardoSantos2@users.noreply.github.com>
2022-04-13 14:30:19 +0100

    Update jwks_client.py
    If the public key use of the JWK is encryption, this code does not work. Is there a reason for that?
    https://datatracker.ietf.org/doc/html/rfc7517#section-4.2

--- jwt/jwks_client.py
+++ jwt/jwks_client.py
@@ -29,7 +29,7 @@ class PyJWKClient:
         signing_keys = [
             jwk_set_key
             for jwk_set_key in jwk_set.keys
-            if jwk_set_key.public_key_use in ["sig", None] and jwk_set_key.key_id
+            if jwk_set_key.public_key_use in ["sig", "enc", None] and jwk_set_key.key_id
         ]
 
         if not signing_keys:
