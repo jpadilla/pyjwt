d6870f14394eb9216f38c172fddddb395474dffc
Stephen Rosen <sirosen@globus.org>
2022-07-03 15:44:00 +0000

    Add compute_hash_digest to Algorithm objects
    `Algorithm.compute_hash_digest` is defined as a method which inspects
    the object to see that it has the requisite attributes, `hash_alg`.
    
    If `hash_alg` is not set, then the method raises a
    NotImplementedError. This applies to classes like NoneAlgorithm.
    
    If `hash_alg` is set, then it is checked for
    ```
    has_crypto  # is cryptography available?
    and isinstance(hash_alg, type)
    and issubclass(hash_alg, hashes.HashAlgorithm)
    ```
    to see which API for computing a digest is appropriate --
    `hashlib` vs `cryptography.hazmat.primitives.hashes`.
    
    These checks could be avoided at runtime if it were necessary to
    optimize further (e.g. attach compute_hash_digest methods to classes
    with a class decorator) but this is not clearly a worthwhile
    optimization. Such perf tuning is intentionally omitted for now.

--- jwt/algorithms.py
+++ jwt/algorithms.py
@@ -18,6 +18,7 @@ from .utils import (
 try:
     import cryptography.exceptions
     from cryptography.exceptions import InvalidSignature
+    from cryptography.hazmat.backends import default_backend
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.asymmetric import ec, padding
     from cryptography.hazmat.primitives.asymmetric.ec import (
@@ -110,6 +111,28 @@ class Algorithm:
     """
     The interface for an algorithm used to sign and verify tokens.
     """
+
+    def compute_hash_digest(self, bytestr: bytes) -> bytes:
+        """
+        Compute a hash digest using the specified algorithm's hash algorithm.
+
+        If there is no hash algorithm, raises a NotImplementedError.
+        """
+        # lookup self.hash_alg if defined in a way that mypy can understand
+        hash_alg = getattr(self, "hash_alg", None)
+        if hash_alg is None:
+            raise NotImplementedError
+
+        if (
+            has_crypto
+            and isinstance(hash_alg, type)
+            and issubclass(hash_alg, hashes.HashAlgorithm)
+        ):
+            digest = hashes.Hash(hash_alg(), backend=default_backend())
+            digest.update(bytestr)
+            return digest.finalize()
+        else:
+            return hash_alg(bytestr).digest()
 
     def prepare_key(self, key):
         """
