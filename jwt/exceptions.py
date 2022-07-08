b7d3a32d8633a68e8566d25a5a2c13902fee039c
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-08 10:33:33 -0400

    refactor: add type hints to exceptions

--- jwt/exceptions.py
+++ jwt/exceptions.py
@@ -47,10 +47,10 @@ class InvalidAlgorithmError(InvalidTokenError):
 
 
 class MissingRequiredClaimError(InvalidTokenError):
-    def __init__(self, claim):
+    def __init__(self, claim: str) -> None:
         self.claim = claim
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Token is missing the "{self.claim}" claim'
 
 
