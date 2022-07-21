9ac51d92907b1090997bc74ad3788dccce2f9cf8
JulianMaurin <julian.maurin.dev+github@proton.me>
2022-07-21 17:54:43 +0200

    feat(test_api_jwt): ensure audience as bytes raises error

--- tests/test_api_jwt.py
+++ tests/test_api_jwt.py
@@ -419,6 +419,12 @@ class TestJWT:
         with pytest.raises(InvalidAudienceError):
             jwt.decode(token, "secret", audience="urn-me", algorithms=["HS256"])
 
+    def test_raise_exception_audience_as_bytes(self, jwt):
+        payload = {"some": "payload", "aud": ["urn:me", "urn:someone-else"]}
+        token = jwt.encode(payload, "secret")
+        with pytest.raises(InvalidAudienceError):
+            jwt.decode(token, "secret", audience="urn:me".encode(), algorithms=["HS256"])
+
     def test_raise_exception_invalid_audience_in_array(self, jwt):
         payload = {
             "some": "payload",
