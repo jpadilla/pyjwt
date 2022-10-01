2aeeaf27ee7c4ec0c7d01c5ec855c2634efc8b7e
Maxim <me@alzhanov.ru>
2022-10-02 02:32:44 +0300

    Differentiate between two errors

--- jwt/api_jwt.py
+++ jwt/api_jwt.py
@@ -259,7 +259,7 @@ class PyJWT:
             audience = [audience]
 
         if all(aud not in audience_claims for aud in audience):
-            raise InvalidAudienceError("Invalid audience")
+            raise InvalidAudienceError("Audience doesn't match")
 
     def _validate_iss(self, payload, issuer):
         if issuer is None:
