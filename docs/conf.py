cf6c922557740ffd4e71610a836177b498912ca0
José Padilla <jpadilla@users.noreply.github.com>
2024-10-07 16:23:59 -0400

    fix more docs

--- docs/conf.py
+++ docs/conf.py
@@ -95,12 +95,11 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_context = {
-    "extra_css_files": [
-        # override wide tables in RTD theme
-        "_static/theme_overrides.css"
-    ]
-}
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+     "theme_overrides.css",
+]
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "PyJWTdoc"
